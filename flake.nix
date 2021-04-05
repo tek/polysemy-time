08c9569cfb47b065808c76ef1d321726a67d2b2f
Torsten Schmits <torstenschmits@gmail.com>
2021-04-05 19:42:31 +0200

    update version bounds

--- flake.nix
+++ flake.nix
@@ -20,6 +20,7 @@
       tasty-hedgehog = hackage "1.1.0.0" "0cs96s7z5csrlwj334v8zl459j5s4ws6gmjh59cv01wwvvrrjwd9";
       polysemy = hackage "1.5.0.0" "1xl472xqdxnp4ysyqnackpfn6wbx03rlgwmy9907bklrh557il6d";
       polysemy-plugin = hackage "0.3.0.0" "1frz0iksmg8bpm7ybnpz9h75hp6hajd20vpdvmi04aspklmr6hj0";
+      relude = hackage "1.0.0.1" "164p21334c3pyfzs839cv90438naxq9pmpyvy87113mwy51gm6xn";
       chronos = source.root chronos;
       polysemy-test = source.sub polysemy-test "packages/polysemy-test";
     };
