699cead57f6e15971f63be5a063dff1fca8d83e5
Torsten Schmits <torstenschmits@gmail.com>
2021-04-15 19:38:47 +0200

    do not use master for polysemy-test

--- flake.nix
+++ flake.nix
@@ -2,31 +2,30 @@
   description = "Polysemy effect for time";
 
   inputs = {
+    tryp-hs.url = github:tek/tryp-hs;
     chronos = { url = github:andrewthad/chronos; flake = false; };
-    polysemy-test.url = github:tek/polysemy-test;
   };
 
-  outputs = { chronos, polysemy-test, ...}@inputs:
+  outputs = { tryp-hs, chronos, ...}@inputs:
   let
-    inherit (polysemy-test.inputs) tryp-hs;
-    overrides = { hackage, source, ... }: {
+    common = { hackage, ... }: {
+      polysemy-test = hackage "0.3.1.1" "0x0zg1kljr7a1mwmm3zrmha5inz3l2pkldnq65fvsig8f3x8rsar";
+    };
+
+    main = { hackage, source, ... }: {
       path = hackage "0.8.0" "0isldidz2gypw2pz399g6rn77x9mppd1mvj5h6ify4pj4mpla0pb";
       tasty-hedgehog = hackage "1.1.0.0" "0cs96s7z5csrlwj334v8zl459j5s4ws6gmjh59cv01wwvvrrjwd9";
       polysemy = hackage "1.5.0.0" "1xl472xqdxnp4ysyqnackpfn6wbx03rlgwmy9907bklrh557il6d";
       polysemy-plugin = hackage "0.3.0.0" "1frz0iksmg8bpm7ybnpz9h75hp6hajd20vpdvmi04aspklmr6hj0";
       relude = hackage "1.0.0.1" "164p21334c3pyfzs839cv90438naxq9pmpyvy87113mwy51gm6xn";
       chronos = source.root chronos;
-      polysemy-test = source.sub polysemy-test "packages/polysemy-test";
-    };
-
-    compatOverrides = { hackage, ... }: {
-      polysemy-test = hackage "0.3.1.1" "0x0zg1kljr7a1mwmm3zrmha5inz3l2pkldnq65fvsig8f3x8rsar";
     };
   in
   tryp-hs.flake {
     base = ./.;
     main = "polysemy-chronos";
-    inherit overrides compatOverrides;
+    overrides = [common main];
+    compatOverrides = common;
     packages = {
       polysemy-time = "packages/time";
       polysemy-chronos = "packages/chronos";
