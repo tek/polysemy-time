f9de505a74217a7ecf405e7cefe30fec2cb92399
Torsten Schmits <git@tryp.io>
2021-12-12 21:58:56 +0100

    bump

--- flake.nix
+++ flake.nix
@@ -5,34 +5,26 @@
 
   outputs = { hix, ...}@inputs:
   let
-    compat901 = { hackage, source, jailbreak, minimal, noHpack, ... }: {
+    ghc901 = { hackage, source, jailbreak, minimal, noHpack, ... }: {
       path = hackage "0.9.0" "14symzl1rszvk5zivv85k79anz7xyl5gaxy0sm4vhhzsgxc59msv";
       path-io = jailbreak (hackage "1.6.3" "05hcxgyf6kkz36mazd0fqwb6mjy2049gx3vh8qq9h93gfjkpp2vc");
       relude = hackage "1.0.0.1" "164p21334c3pyfzs839cv90438naxq9pmpyvy87113mwy51gm6xn";
       type-errors-pretty = jailbreak;
     };
 
-    common = { hackage, ... }: {
+    all = { hackage, ... }: {
       polysemy = hackage "1.6.0.0" "15k51ysrfcbkww1562g8zvrlzymlk2rxhcsz9ipsb0q6h571qgvf";
       polysemy-test = hackage "0.3.1.7" "0j33f5zh6gyhl86w8kqh6nm02915b4n32xikxc4hwcy7p5l7cl34";
-    };
-
-    main = { hackage, source, minimal, ... }: {
-      path = hackage "0.8.0" "0isldidz2gypw2pz399g6rn77x9mppd1mvj5h6ify4pj4mpla0pb";
-      tasty-hedgehog = hackage "1.1.0.0" "0cs96s7z5csrlwj334v8zl459j5s4ws6gmjh59cv01wwvvrrjwd9";
-      relude = hackage "1.0.0.1" "164p21334c3pyfzs839cv90438naxq9pmpyvy87113mwy51gm6xn";
     };
   in
   hix.flake {
     base = ./.;
     main = "polysemy-chronos";
-    overrides = [common main];
-    compatOverrides = { all = [common]; ghc901 = [compat901]; };
+    overrides = { inherit all ghc901; };
     packages = {
-      polysemy-time = "packages/time";
-      polysemy-chronos = "packages/chronos";
+      polysemy-time = ./packages/time;
+      polysemy-chronos = ./packages/chronos;
     };
     versionFile = "ops/hpack/shared/meta.yaml";
-    ghcid.easy-hls = false;
   };
 }
