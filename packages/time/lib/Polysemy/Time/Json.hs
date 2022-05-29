781b3a948b8934db02ff05c29a1ff7fb2ff5594f
Torsten Schmits <git@tryp.io>
2022-05-30 01:45:54 +0200

    tweak json generators

--- packages/time/lib/Polysemy/Time/Json.hs
+++ packages/time/lib/Polysemy/Time/Json.hs
@@ -3,12 +3,20 @@ module Polysemy.Time.Json where
 
 import qualified Data.Aeson as Aeson
 import Data.Aeson.TH (deriveJSON)
+import Data.List (dropWhileEnd)
 import qualified Language.Haskell.TH.Syntax as TH
 
--- |Derive Aeson codecs with custom settings.
+-- |Aeson codec options that remove leading and trailing underscores.
+basicOptions :: Aeson.Options
+basicOptions =
+  Aeson.defaultOptions { Aeson.fieldLabelModifier = dropWhileEnd ('_' ==) . dropWhile ('_' ==) }
+
+-- |Derive Aeson codecs that strip underscores and unwrap data/newtype with single fields.
 json :: TH.Name -> TH.Q [TH.Dec]
 json =
-  deriveJSON Aeson.defaultOptions {
-    Aeson.fieldLabelModifier = dropWhile ('_' ==),
-    Aeson.unwrapUnaryRecords = True
-  }
+  deriveJSON basicOptions { Aeson.unwrapUnaryRecords = True }
+
+-- |Derive Aeson codecs that strip underscores.
+unaryJson :: TH.Name -> TH.Q [TH.Dec]
+unaryJson =
+  deriveJSON basicOptions
