ff848b19ca0984dda4b2ca79996596f2f1852399
tek <tek@users.noreply.github.com>
2024-05-26 13:44:15 +0000

    Bump polysemy-test
    New versions:
    
    * polysemy-test-0.3.1.6 [>=0.3.1.6]

--- ops/managed.nix
+++ ops/managed.nix
@@ -14,7 +14,7 @@
         upper = null;
       };
       polysemy-test = {
-        lower = "0.6.0.0";
+        lower = "0.3.1.6";
         upper = null;
       };
       tasty = {
@@ -36,7 +36,7 @@
         upper = null;
       };
       polysemy-test = {
-        lower = "0.6.0.0";
+        lower = "0.3.1.6";
         upper = null;
       };
       tasty = {
@@ -64,7 +64,7 @@
       base = "4.17.2.1";
       chronos = "1.1.5";
       incipit-core = "0.4.1.0";
-      polysemy-test = "0.6.0.0";
+      polysemy-test = "0.3.1.6";
       tasty = "1.4.2";
       template-haskell = "2.19.0.0";
       time = "1.12.2";
@@ -85,6 +85,10 @@
   };
   overrides = {
     lower = {
+      cabal-doctest = {
+        version = "1.0.9";
+        hash = "0irxfxy1qw7sif4408xdhqycddb4hs3hcf6xfxm65glsnmnmwl2i";
+      };
       chronos = {
         version = "1.1.5";
         hash = "1rja3hsf7s4gfm1xq5s2gydx9d0jckcjpza9rqvhbpv1h0qsh3f3";
@@ -92,6 +96,14 @@
       clock = {
         version = "0.8.4";
         hash = "14gy1a16l5s70pyqlsmylxsiiagas2yflqmjjmrdbzj4g1zxy39r";
+      };
+      either = {
+        version = "5.0.2";
+        hash = "1dcl2pg45cbjygbxasn0c7qfs4svrmi7fdp9xl0ivxpgg0y648d0";
+      };
+      ghc-tcplugins-extra = {
+        version = "0.4.6";
+        hash = "0d0b6h23yq4jccv00wgikxr7hqprkf8afzxcy1mrlgwa9158sl3l";
       };
       incipit-base = {
         version = "0.4.1.0";
@@ -101,13 +113,33 @@
         version = "0.4.1.0";
         hash = "1fm6bf1w8mvpa9qlgxqv3ngf0lyb3057cwv5ajibgbljjaznxpxc";
       };
+      polysemy-plugin = {
+        version = "0.4.5.2";
+        hash = "18y0nfx7x7am7cvj9wwhzal9bqv6sj7ckvmkd16blx4c2nqyikp9";
+      };
       polysemy-test = {
-        version = "0.6.0.0";
-        hash = "07pi549ral22sxhja67k5b9v787q0b32ysp0bq9szhwjqgxsab46";
+        version = "0.3.1.6";
+        hash = "0bfh37l68a5chhjfr7gqcffsmvdgg5hqclxi0fc5xnqni2mg81ak";
+      };
+      profunctors = {
+        version = "5.6.2";
+        hash = "19gnn2br16x52lmvfjj06xblsks0wz3081z0jf0jydncbgm6kfan";
+      };
+      relude = {
+        version = "1.2.1.0";
+        hash = "0scfivrq6p3hlqkj49kh9m9l8nkwbq339bcpc4rrsd0fq9b8ripc";
       };
       semigroups = {
         version = "0.20";
         hash = "03llc5y6zpzfn9hy71fg2kj7pipip4fphmns9yx47xmryn111d4g";
+      };
+      split = {
+        version = "0.2.5";
+        hash = "0l9rhscr8rj968qhqhwzf18mddp5d5c22cwwhzb72q646wxmwnh8";
+      };
+      string-interpolate = {
+        version = "0.3.3.0";
+        hash = "01hwj10v0xdhxacrlnbaan9dplrp60axfg4z5ny2czx2psigfc0n";
       };
       tasty = {
         version = "1.4.2";
@@ -117,6 +149,10 @@
         version = "1.4.0.2";
         hash = "04kg2qdnsqzzmj3xggy2jcgidlp21lsjkz4sfnbq7b1yhrv2vbbc";
       };
+      text-conversions = {
+        version = "0.3.1.1";
+        hash = "0lfcp2f8ld46cry5wm2afcn362mb7fp28ii3afji7bns1fvhh6lf";
+      };
       torsor = {
         version = "0.1";
         hash = "07p05f7j6h99pxx1v4j0ya5py4nc93gnbj09pdhl23i2zc75064f";
@@ -124,6 +160,10 @@
       unbounded-delays = {
         version = "0.1.1.1";
         hash = "1kbh2yr7lwzrhjniyfllsix2zn8bmz9yrkhnq5lxv9ic9bbxnls7";
+      };
+      utf8-string = {
+        version = "1.0.2";
+        hash = "09i87djx45cinql3mg222m3li08wn8hgmdcp2vqrqc8pn437azrh";
       };
       wcwidth = {
         version = "0.0.2";
--- ops/overrides.nix
+++ ops/overrides.nix
@@ -12,6 +12,495 @@ hls = {
 latest = {
 };
 lower = {
+  cabal-doctest = {
+  meta = {
+    sha256 = "0irxfxy1qw7sif4408xdhqycddb4hs3hcf6xfxm65glsnmnmwl2i";
+    ver = "1.0.9";
+  };
+  drv = { mkDerivation, base, Cabal, directory, filepath, lib }:
+mkDerivation {
+  pname = "cabal-doctest";
+  version = "1.0.9";
+  src = /nix/store/zvv4lgrqgjx826ryk6697617pd3xpr7s-source;
+  libraryHaskellDepends = [ base Cabal directory filepath ];
+  homepage = "https://github.com/haskellari/cabal-doctest";
+  description = "A Setup.hs helper for running doctests";
+  license = lib.licenses.bsd3;
+}
+;
+}
+;
+  chronos = {
+  meta = {
+    sha256 = "1rja3hsf7s4gfm1xq5s2gydx9d0jckcjpza9rqvhbpv1h0qsh3f3";
+    ver = "1.1.5";
+  };
+  drv = { mkDerivation, aeson, attoparsec, base, bytebuild, byteslice
+, bytesmith, bytestring, criterion, deepseq, hashable, HUnit, lib
+, natural-arithmetic, old-locale, primitive, QuickCheck, semigroups
+, test-framework, test-framework-hunit, test-framework-quickcheck2
+, text, text-short, thyme, time, torsor, vector
+}:
+mkDerivation {
+  pname = "chronos";
+  version = "1.1.5";
+  src = /nix/store/siki35315x8lmjvd0wmrsavbygh4p6f0-source;
+  libraryHaskellDepends = [
+    aeson attoparsec base bytebuild byteslice bytesmith bytestring
+    deepseq hashable natural-arithmetic primitive semigroups text
+    text-short torsor vector
+  ];
+  testHaskellDepends = [
+    aeson attoparsec base bytestring deepseq HUnit QuickCheck
+    test-framework test-framework-hunit test-framework-quickcheck2 text
+    torsor
+  ];
+  benchmarkHaskellDepends = [
+    attoparsec base bytestring criterion deepseq old-locale QuickCheck
+    text text-short thyme time vector
+  ];
+  homepage = "https://github.com/andrewthad/chronos";
+  description = "A high-performance time library";
+  license = lib.licenses.bsd3;
+}
+;
+}
+;
+  clock = {
+  meta = {
+    sha256 = "14gy1a16l5s70pyqlsmylxsiiagas2yflqmjjmrdbzj4g1zxy39r";
+    ver = "0.8.4";
+  };
+  drv = { mkDerivation, base, criterion, lib, tasty, tasty-quickcheck }:
+mkDerivation {
+  pname = "clock";
+  version = "0.8.4";
+  src = /nix/store/rbsi64yfx248l6pmqqvfviaj5alfpfhw-source;
+  libraryHaskellDepends = [ base ];
+  testHaskellDepends = [ base tasty tasty-quickcheck ];
+  benchmarkHaskellDepends = [ base criterion ];
+  homepage = "https://github.com/corsis/clock";
+  description = "High-resolution clock functions: monotonic, realtime, cputime";
+  license = lib.licenses.bsd3;
+}
+;
+}
+;
+  either = {
+  meta = {
+    sha256 = "1dcl2pg45cbjygbxasn0c7qfs4svrmi7fdp9xl0ivxpgg0y648d0";
+    ver = "5.0.2";
+  };
+  drv = { mkDerivation, base, bifunctors, lib, mtl, profunctors, QuickCheck
+, semigroupoids, test-framework, test-framework-quickcheck2
+}:
+mkDerivation {
+  pname = "either";
+  version = "5.0.2";
+  src = /nix/store/x3ab6z30yvdny71zxlpy0fqa1k74if1d-source;
+  libraryHaskellDepends = [
+    base bifunctors mtl profunctors semigroupoids
+  ];
+  testHaskellDepends = [
+    base QuickCheck test-framework test-framework-quickcheck2
+  ];
+  homepage = "http://github.com/ekmett/either/";
+  description = "Combinators for working with sums";
+  license = lib.licenses.bsd3;
+}
+;
+}
+;
+  ghc-tcplugins-extra = {
+  meta = {
+    sha256 = "0d0b6h23yq4jccv00wgikxr7hqprkf8afzxcy1mrlgwa9158sl3l";
+    ver = "0.4.6";
+  };
+  drv = { mkDerivation, base, ghc, lib }:
+mkDerivation {
+  pname = "ghc-tcplugins-extra";
+  version = "0.4.6";
+  src = /nix/store/hd335h1j3d7i5gj92jjkp3s4k4qnd1lg-source;
+  libraryHaskellDepends = [ base ghc ];
+  homepage = "https://github.com/clash-lang/ghc-tcplugins-extra#readme";
+  description = "Utilities for writing GHC type-checker plugins";
+  license = lib.licenses.bsd2;
+}
+;
+}
+;
+  incipit-base = {
+  meta = {
+    sha256 = "17579j3hzsh3ic0272h8ly8k7gz4zm1hv5jqimdam9gcq8alahkl";
+    ver = "0.4.1.0";
+  };
+  drv = { mkDerivation, base, bytestring, containers, data-default, lib
+, stm, text
+}:
+mkDerivation {
+  pname = "incipit-base";
+  version = "0.4.1.0";
+  src = /nix/store/9zk69qkk9paqair15m9b1sc2bbb9220a-source;
+  libraryHaskellDepends = [
+    base bytestring containers data-default stm text
+  ];
+  homepage = "https://github.com/tek/incipit-core#readme";
+  description = "A Prelude for Polysemy – Base Reexports";
+  license = "BSD-2-Clause-Patent";
+}
+;
+}
+;
+  incipit-core = {
+  meta = {
+    sha256 = "1fm6bf1w8mvpa9qlgxqv3ngf0lyb3057cwv5ajibgbljjaznxpxc";
+    ver = "0.4.1.0";
+  };
+  drv = { mkDerivation, base, incipit-base, lib, polysemy }:
+mkDerivation {
+  pname = "incipit-core";
+  version = "0.4.1.0";
+  src = /nix/store/cyi2s97p8jcj6mr8ci7mvbfwpfnzii0i-source;
+  libraryHaskellDepends = [ base incipit-base polysemy ];
+  homepage = "https://github.com/tek/incipit-core#readme";
+  description = "A Prelude for Polysemy";
+  license = "BSD-2-Clause-Patent";
+}
+;
+}
+;
+  polysemy-plugin = {
+  meta = {
+    sha256 = "18y0nfx7x7am7cvj9wwhzal9bqv6sj7ckvmkd16blx4c2nqyikp9";
+    ver = "0.4.5.2";
+  };
+  drv = { mkDerivation, base, Cabal, cabal-doctest, containers, doctest
+, ghc, ghc-tcplugins-extra, hspec, hspec-discover
+, inspection-testing, lib, polysemy, should-not-typecheck, syb
+, transformers
+}:
+mkDerivation {
+  pname = "polysemy-plugin";
+  version = "0.4.5.2";
+  src = /nix/store/l68cq0g74sq0ws1plbgva2ry1psk5s21-source;
+  setupHaskellDepends = [ base Cabal cabal-doctest ];
+  libraryHaskellDepends = [
+    base containers ghc ghc-tcplugins-extra polysemy syb transformers
+  ];
+  testHaskellDepends = [
+    base containers doctest ghc ghc-tcplugins-extra hspec
+    hspec-discover inspection-testing polysemy should-not-typecheck syb
+    transformers
+  ];
+  testToolDepends = [ hspec-discover ];
+  homepage = "https://github.com/polysemy-research/polysemy#readme";
+  description = "Disambiguate obvious uses of effects";
+  license = lib.licenses.bsd3;
+}
+;
+}
+;
+  polysemy-test = {
+  meta = {
+    sha256 = "0bfh37l68a5chhjfr7gqcffsmvdgg5hqclxi0fc5xnqni2mg81ak";
+    ver = "0.3.1.6";
+  };
+  drv = { mkDerivation, base, containers, either, hedgehog, lib, path
+, path-io, polysemy, polysemy-plugin, relude, string-interpolate
+, tasty, tasty-hedgehog, template-haskell, text, transformers
+}:
+mkDerivation {
+  pname = "polysemy-test";
+  version = "0.3.1.6";
+  src = /nix/store/w39a7g4c71xqi0n2h0szy720vmjcigwj-source;
+  enableSeparateDataOutput = true;
+  libraryHaskellDepends = [
+    base containers either hedgehog path path-io polysemy
+    polysemy-plugin relude string-interpolate tasty tasty-hedgehog
+    template-haskell text transformers
+  ];
+  testHaskellDepends = [
+    base containers either hedgehog path path-io polysemy
+    polysemy-plugin relude string-interpolate tasty tasty-hedgehog
+    template-haskell text transformers
+  ];
+  description = "Polysemy effects for testing";
+  license = "BSD-2-Clause-Patent";
+}
+;
+}
+;
+  profunctors = {
+  meta = {
+    sha256 = "19gnn2br16x52lmvfjj06xblsks0wz3081z0jf0jydncbgm6kfan";
+    ver = "5.6.2";
+  };
+  drv = { mkDerivation, base, base-orphans, bifunctors, comonad
+, contravariant, distributive, lib, tagged, transformers
+}:
+mkDerivation {
+  pname = "profunctors";
+  version = "5.6.2";
+  src = /nix/store/d5lsaarmw7n5v0y6kd896z4sd9nnx7b3-source;
+  libraryHaskellDepends = [
+    base base-orphans bifunctors comonad contravariant distributive
+    tagged transformers
+  ];
+  homepage = "http://github.com/ekmett/profunctors/";
+  description = "Profunctors";
+  license = lib.licenses.bsd3;
+}
+;
+}
+;
+  relude = {
+  meta = {
+    sha256 = "0scfivrq6p3hlqkj49kh9m9l8nkwbq339bcpc4rrsd0fq9b8ripc";
+    ver = "1.2.1.0";
+  };
+  drv = { mkDerivation, base, bytestring, containers, deepseq, doctest
+, ghc-prim, Glob, hashable, hedgehog, lib, mtl, stm, tasty-bench
+, text, transformers, unordered-containers
+}:
+mkDerivation {
+  pname = "relude";
+  version = "1.2.1.0";
+  src = /nix/store/y4q0h2qhd4bi2czgvshclhla0hdxgj7z-source;
+  libraryHaskellDepends = [
+    base bytestring containers deepseq ghc-prim hashable mtl stm text
+    transformers unordered-containers
+  ];
+  testHaskellDepends = [
+    base bytestring containers doctest Glob hedgehog text
+  ];
+  benchmarkHaskellDepends = [
+    base tasty-bench unordered-containers
+  ];
+  homepage = "https://github.com/kowainik/relude";
+  description = "Safe, performant, user-friendly and lightweight Haskell Standard Library";
+  license = lib.licenses.mit;
+}
+;
+}
+;
+  semigroups = {
+  meta = {
+    sha256 = "03llc5y6zpzfn9hy71fg2kj7pipip4fphmns9yx47xmryn111d4g";
+    ver = "0.20";
+  };
+  drv = { mkDerivation, base, lib }:
+mkDerivation {
+  pname = "semigroups";
+  version = "0.20";
+  src = /nix/store/d66bw65cxzc4q2yv87pa8shqbwvh87ny-source;
+  libraryHaskellDepends = [ base ];
+  homepage = "http://github.com/ekmett/semigroups/";
+  description = "Anything that associates";
+  license = lib.licenses.bsd3;
+}
+;
+}
+;
+  split = {
+  meta = {
+    sha256 = "0l9rhscr8rj968qhqhwzf18mddp5d5c22cwwhzb72q646wxmwnh8";
+    ver = "0.2.5";
+  };
+  drv = { mkDerivation, base, lib, QuickCheck }:
+mkDerivation {
+  pname = "split";
+  version = "0.2.5";
+  src = /nix/store/kfc5qwwxki2b6gnk4ka7fyavf1ahcfjc-source;
+  libraryHaskellDepends = [ base ];
+  testHaskellDepends = [ base QuickCheck ];
+  description = "Combinator library for splitting lists";
+  license = lib.licenses.bsd3;
+}
+;
+}
+;
+  string-interpolate = {
+  meta = {
+    sha256 = "01hwj10v0xdhxacrlnbaan9dplrp60axfg4z5ny2czx2psigfc0n";
+    ver = "0.3.3.0";
+  };
+  drv = { mkDerivation, base, bytestring, criterion, deepseq, formatting
+, haskell-src-exts, haskell-src-meta, hspec, hspec-core
+, interpolate, lib, neat-interpolation, QuickCheck
+, quickcheck-instances, quickcheck-text, quickcheck-unicode, split
+, template-haskell, text, text-conversions, unordered-containers
+, utf8-string
+}:
+mkDerivation {
+  pname = "string-interpolate";
+  version = "0.3.3.0";
+  src = /nix/store/khbc30bhz3mz8ba425i073jhmxy026rm-source;
+  libraryHaskellDepends = [
+    base bytestring haskell-src-exts haskell-src-meta split
+    template-haskell text text-conversions utf8-string
+  ];
+  testHaskellDepends = [
+    base bytestring hspec hspec-core QuickCheck quickcheck-instances
+    quickcheck-text quickcheck-unicode template-haskell text
+    unordered-containers
+  ];
+  benchmarkHaskellDepends = [
+    base bytestring criterion deepseq formatting interpolate
+    neat-interpolation QuickCheck text
+  ];
+  homepage = "https://gitlab.com/williamyaoh/string-interpolate/blob/master/README.md";
+  description = "Haskell string/text/bytestring interpolation that just works";
+  license = lib.licenses.bsd3;
+}
+;
+}
+;
+  tasty = {
+  meta = {
+    sha256 = "0jdr0r9x1apxfma0p5rvyai7sd9wsg22hblzcrxmw7lq34j1606f";
+    ver = "1.4.2";
+  };
+  drv = { mkDerivation, ansi-terminal, base, clock, containers, lib, mtl
+, optparse-applicative, stm, tagged, unbounded-delays, unix
+, wcwidth
+}:
+mkDerivation {
+  pname = "tasty";
+  version = "1.4.2";
+  src = /nix/store/40r5abgjm3rrhq28ji8qldmnpaq9idda-source;
+  libraryHaskellDepends = [
+    ansi-terminal base clock containers mtl optparse-applicative stm
+    tagged unbounded-delays unix wcwidth
+  ];
+  homepage = "https://github.com/UnkindPartition/tasty";
+  description = "Modern and extensible testing framework";
+  license = lib.licenses.mit;
+}
+;
+}
+;
+  tasty-hedgehog = {
+  meta = {
+    sha256 = "04kg2qdnsqzzmj3xggy2jcgidlp21lsjkz4sfnbq7b1yhrv2vbbc";
+    ver = "1.4.0.2";
+  };
+  drv = { mkDerivation, base, hedgehog, lib, tagged, tasty
+, tasty-expected-failure
+}:
+mkDerivation {
+  pname = "tasty-hedgehog";
+  version = "1.4.0.2";
+  src = /nix/store/b9mxq4fh65sif22q9a4g041jvp847cyc-source;
+  libraryHaskellDepends = [ base hedgehog tagged tasty ];
+  testHaskellDepends = [
+    base hedgehog tasty tasty-expected-failure
+  ];
+  homepage = "https://github.com/qfpl/tasty-hedgehog";
+  description = "Integration for tasty and hedgehog";
+  license = lib.licenses.bsd3;
+}
+;
+}
+;
+  text-conversions = {
+  meta = {
+    sha256 = "0lfcp2f8ld46cry5wm2afcn362mb7fp28ii3afji7bns1fvhh6lf";
+    ver = "0.3.1.1";
+  };
+  drv = { mkDerivation, base, base16-bytestring, base64-bytestring
+, bytestring, hspec, hspec-discover, lib, text
+}:
+mkDerivation {
+  pname = "text-conversions";
+  version = "0.3.1.1";
+  src = /nix/store/jwh6vj5a4l3kbhlvicra77pg738sskrv-source;
+  libraryHaskellDepends = [
+    base base16-bytestring base64-bytestring bytestring text
+  ];
+  testHaskellDepends = [ base bytestring hspec text ];
+  testToolDepends = [ hspec-discover ];
+  homepage = "https://github.com/cjdev/text-conversions";
+  description = "Safe conversions between textual types";
+  license = lib.licenses.isc;
+}
+;
+}
+;
+  torsor = {
+  meta = {
+    sha256 = "07p05f7j6h99pxx1v4j0ya5py4nc93gnbj09pdhl23i2zc75064f";
+    ver = "0.1";
+  };
+  drv = { mkDerivation, base, lib }:
+mkDerivation {
+  pname = "torsor";
+  version = "0.1";
+  src = /nix/store/320q6gfv9fnpxnxcrckmsblkqvirm8bp-source;
+  libraryHaskellDepends = [ base ];
+  homepage = "https://github.com/andrewthad/torsor#readme";
+  description = "Torsor Typeclass";
+  license = lib.licenses.bsd3;
+}
+;
+}
+;
+  unbounded-delays = {
+  meta = {
+    sha256 = "1kbh2yr7lwzrhjniyfllsix2zn8bmz9yrkhnq5lxv9ic9bbxnls7";
+    ver = "0.1.1.1";
+  };
+  drv = { mkDerivation, base, lib }:
+mkDerivation {
+  pname = "unbounded-delays";
+  version = "0.1.1.1";
+  src = /nix/store/hp6mlfj9kg8r7l4gjcak2i7zbxwjzl4s-source;
+  libraryHaskellDepends = [ base ];
+  homepage = "https://github.com/basvandijk/unbounded-delays";
+  description = "Unbounded thread delays and timeouts";
+  license = lib.licenses.bsd3;
+}
+;
+}
+;
+  utf8-string = {
+  meta = {
+    sha256 = "09i87djx45cinql3mg222m3li08wn8hgmdcp2vqrqc8pn437azrh";
+    ver = "1.0.2";
+  };
+  drv = { mkDerivation, base, bytestring, HUnit, lib }:
+mkDerivation {
+  pname = "utf8-string";
+  version = "1.0.2";
+  src = /nix/store/q2k5852xjw7pmz417sv4s9fjlddh2ami-source;
+  libraryHaskellDepends = [ base bytestring ];
+  testHaskellDepends = [ base HUnit ];
+  homepage = "https://github.com/glguy/utf8-string/";
+  description = "Support for reading and writing UTF8 Strings";
+  license = lib.licenses.bsd3;
+}
+;
+}
+;
+  wcwidth = {
+  meta = {
+    sha256 = "0131h9vg8dvrqcc2sn0k8y6cb08fazlfhr4922hwv2vbx3cnyy3z";
+    ver = "0.0.2";
+  };
+  drv = { mkDerivation, base, containers, lib }:
+mkDerivation {
+  pname = "wcwidth";
+  version = "0.0.2";
+  src = /nix/store/n4f5lcschfan070bj3i6fa8pwjx1p94s-source;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [ base containers ];
+  homepage = "http://github.com/solidsnack/wcwidth/";
+  description = "Native wcwidth";
+  license = lib.licenses.bsd3;
+}
+;
+}
+;
 };
 min = {
 };
