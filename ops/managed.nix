6089ef1295df9b691bfff348ada59e4dab0c9e53
tek <tek@users.noreply.github.com>
2024-05-26 13:20:30 +0000

    Bump 9 dependencies
    New versions:
    
    * aeson-2.1.2.1 [>=2.1.2.1]
    * base-4.17.2.1 [>=4.17.2.1]
    * chronos-1.1.5 [>=1.1.5]
    * incipit-core-0.4.1.0 [>=0.4.1.0]
    * polysemy-test-0.6.0.0 [>=0.6.0.0]
    * tasty-1.4.2 [>=1.4.2]
    * template-haskell-2.19.0.0 [>=2.19.0.0]
    * time-1.12.2 [>=1.12.2]
    * torsor-0.1 [>=0.1]

---
+++ ops/managed.nix
@@ -0,0 +1,135 @@
+{
+  bounds = {
+    polysemy-chronos = {
+      base = {
+        lower = "4.17.2.1";
+        upper = null;
+      };
+      chronos = {
+        lower = "1.1.5";
+        upper = null;
+      };
+      incipit-core = {
+        lower = "0.4.1.0";
+        upper = null;
+      };
+      polysemy-test = {
+        lower = "0.6.0.0";
+        upper = null;
+      };
+      tasty = {
+        lower = "1.4.2";
+        upper = null;
+      };
+    };
+    polysemy-time = {
+      aeson = {
+        lower = "2.1.2.1";
+        upper = null;
+      };
+      base = {
+        lower = "4.17.2.1";
+        upper = null;
+      };
+      incipit-core = {
+        lower = "0.4.1.0";
+        upper = null;
+      };
+      polysemy-test = {
+        lower = "0.6.0.0";
+        upper = null;
+      };
+      tasty = {
+        lower = "1.4.2";
+        upper = null;
+      };
+      template-haskell = {
+        lower = "2.19.0.0";
+        upper = null;
+      };
+      time = {
+        lower = "1.12.2";
+        upper = null;
+      };
+      torsor = {
+        lower = "0.1";
+        upper = null;
+      };
+    };
+  };
+  versions = {
+    latest = {};
+    lower = {
+      aeson = "2.1.2.1";
+      base = "4.17.2.1";
+      chronos = "1.1.5";
+      incipit-core = "0.4.1.0";
+      polysemy-test = "0.6.0.0";
+      tasty = "1.4.2";
+      template-haskell = "2.19.0.0";
+      time = "1.12.2";
+      torsor = "0.1";
+    };
+  };
+  initial = {
+    latest = {};
+    lower = {
+      aeson = "2.1.2.1";
+      chronos = "1.1.5";
+      incipit-core = "0.6.0.0";
+      polysemy-test = "0.7.0.0";
+      tasty = "1.4.2";
+      time = "1.12.2";
+      torsor = "0.1";
+    };
+  };
+  overrides = {
+    lower = {
+      chronos = {
+        version = "1.1.5";
+        hash = "1rja3hsf7s4gfm1xq5s2gydx9d0jckcjpza9rqvhbpv1h0qsh3f3";
+      };
+      clock = {
+        version = "0.8.4";
+        hash = "14gy1a16l5s70pyqlsmylxsiiagas2yflqmjjmrdbzj4g1zxy39r";
+      };
+      incipit-base = {
+        version = "0.4.1.0";
+        hash = "17579j3hzsh3ic0272h8ly8k7gz4zm1hv5jqimdam9gcq8alahkl";
+      };
+      incipit-core = {
+        version = "0.4.1.0";
+        hash = "1fm6bf1w8mvpa9qlgxqv3ngf0lyb3057cwv5ajibgbljjaznxpxc";
+      };
+      polysemy-test = {
+        version = "0.6.0.0";
+        hash = "07pi549ral22sxhja67k5b9v787q0b32ysp0bq9szhwjqgxsab46";
+      };
+      semigroups = {
+        version = "0.20";
+        hash = "03llc5y6zpzfn9hy71fg2kj7pipip4fphmns9yx47xmryn111d4g";
+      };
+      tasty = {
+        version = "1.4.2";
+        hash = "0jdr0r9x1apxfma0p5rvyai7sd9wsg22hblzcrxmw7lq34j1606f";
+      };
+      tasty-hedgehog = {
+        version = "1.4.0.2";
+        hash = "04kg2qdnsqzzmj3xggy2jcgidlp21lsjkz4sfnbq7b1yhrv2vbbc";
+      };
+      torsor = {
+        version = "0.1";
+        hash = "07p05f7j6h99pxx1v4j0ya5py4nc93gnbj09pdhl23i2zc75064f";
+      };
+      unbounded-delays = {
+        version = "0.1.1.1";
+        hash = "1kbh2yr7lwzrhjniyfllsix2zn8bmz9yrkhnq5lxv9ic9bbxnls7";
+      };
+      wcwidth = {
+        version = "0.0.2";
+        hash = "0131h9vg8dvrqcc2sn0k8y6cb08fazlfhr4922hwv2vbx3cnyy3z";
+      };
+    };
+  };
+  resolving = false;
+}
